9567764da6e06511f746ad74bb33e53fa2b9a213
Tim Smith <tim@zerovapor.com>
2012-10-18 21:52:28 -0400

    start

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * A Maintenance package for FuelPHP
+ *
+ * @package		Reddit PHP SDK
+ * @version		1.0
+ * @author		Jonathan LeBlanc
+ * @link		https://github.com/jcleblanc/reddit-php-sdk
+ *
+ */
+
+Autoloader::add_classes(array(
+	'Reddit' 	=> __DIR__.'/classes/reddit.php',
+));
